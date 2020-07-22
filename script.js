e34adfcc322e68a84e7383044193f462730590d8
Brian OLeary <oleary.brian1@gmail.com>
2020-07-22 16:30:11 -0400

    it's working!

--- script.js
+++ script.js
@@ -14,21 +14,24 @@ let fivepm = document.getElementById('17')
 // ["nineam", "tenam", "elevenam", "twelvepm", "onepm", "twopm", "threepm", "fourpm", "fivepm"].foreach(changecolor)
 
 changeAllColor();
-
-
+printActivities();
+console.log(nineam.value = localStorage.getItem(9))
 function saveActivity(a) {
-    console.log(a.value)
-    console.log(a.id)
+    // console.log(a.value)
+    // console.log(a.id)
+    localStorage.setItem(a.id, a.value)
 }
-//  let dayOfWeekEl;
-
-// //initialize
-// currentDate()
-
-// function currentDate(){
-//     let displayDate= dayOfWeekEl;
-//     let dayOfWeekEl =moment().format('dddd');  
-// }
+function printActivities() {
+    nineam.value = localStorage.getItem(9)
+    tenam.value = localStorage.getItem(10)
+    elevenam.value = localStorage.getItem(11)
+    twelvepm.value = localStorage.getItem(12)
+    onepm.value = localStorage.getItem(13)
+    twopm.value = localStorage.getItem(14)
+    threepm.value = localStorage.getItem(15)
+    fourpm.value = localStorage.getItem(16)
+    fivepm.value = localStorage.getItem(17)
+}
 function changeAllColor(){
     changecolor(nineam);
     changecolor(tenam);
@@ -41,24 +44,25 @@ function changeAllColor(){
     changecolor(fivepm);
 }
 function changecolor(i){
-    console.log('changecolor called')
+    
+    // console.log('changecolor called')
     for(j = 0; j < arguments.length; j++){
         
         if (parseInt(i.id) < moment().format('HH')){
-            console.log(i,'before')
+            // console.log(i,'before')
     
             i.classList.remove('future')
             i.classList.remove('present')
             i.classList.add('past')
         }
         else if (parseInt(i.id) > moment().format('HH')){
-            console.log(i,'after')
+            // console.log(i,'after')
             i.classList.add('future')
             i.classList.remove('present')
             i.classList.remove('past')
         }
         else if (parseInt(i.id) == moment().format('HH')) {
-            console.log(i,'during')
+            // console.log(i,'during')
             i.classList.remove('future')
             i.classList.add('present')
             i.classList.remove('past')
